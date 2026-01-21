e4cc005f36f5167314697435603e9874c75ded94
Kartikeya Shukla <kartikeyashukla0907@gmail.com>
2026-01-21 21:29:49 +0530

    Update 06reverseint.py

--- 06reverseint.py
+++ 06reverseint.py
@@ -9,9 +9,10 @@ while b>0:
     b=b//10
     l.append(c)
 print(l)
-num="".join([str(n) for n in l])
-num1=int(num)
+nu="".join([str(n) for n in l])
+num1=int(nu)
 if a<0:
     print(-num1)
 else:
     print(num1)
+
