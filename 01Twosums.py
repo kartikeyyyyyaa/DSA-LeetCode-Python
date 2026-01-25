eb94dd5d27790954d4796d8d66dc86a4cb88e257
Kartikeya Shukla <kartikeyashukla0907@gmail.com>
2026-01-25 18:20:51 +0530

    Update 01Twosums.py

--- 01Twosums.py
+++ 01Twosums.py
@@ -1,14 +1,15 @@
 def twosums():
     nums=list(map(int,input("enter array seperated by comma : ").split()))
     target=int(input( ))
-    list2=[]
+    list3=[]
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j]==target:
-                list2.append(i)
-                list2.append(j)
-                print(list2)
+                list3.append(i)
+                list3.append(j)
+                print(list3)
                 break
             else:
                 continue
 twosums()
+
